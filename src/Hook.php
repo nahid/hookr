31b22ab9bd8155f7932f1283eaa5ac99b745dba8
Nahid Bin Azhar <nahid.dns@gmail.com>
2018-07-15 12:35:29 +0600

    simplify code

--- src/Hook.php
+++ src/Hook.php
@@ -6,6 +6,16 @@ class Hook
 {
     protected static $hookActions = [];
     protected static $hookFilters = [];
+    protected static $instance = null;
+
+    public static function getInstance()
+    {
+        if (is_null(static::$instance)) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
 
     public static function action($name, $params = [])
     {
